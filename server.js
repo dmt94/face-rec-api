1c24a592dfea399b7fc151a3d8ce765de4216c1b
Daevah Taylaran <daevtayl@gmail.com>
2022-10-06 14:05:15 -0700

    more connect

--- server.js
+++ server.js
@@ -19,12 +19,18 @@ const client = new Client({
 
 client.connect();
 
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
+
 const db = knex({
   client: 'pg',
   connection: client
 });
-
-
 
 //query statement, the knex builder made query request
 // db.select('*')
