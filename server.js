5d7f19bac4fe47be103cd98f50bae834cb29b87f
Daevah Taylaran <daevtayl@gmail.com>
2022-10-06 11:51:48 -0700

    removed some express functions

--- server.js
+++ server.js
@@ -32,17 +32,13 @@ database initiates with the default users prior to the new user added
 database is superior bc they run on disk, so that every info is stored efficiently 
 */
 const app = express();
-app.use(express.urlencoded({extended: false}));
-app.use(express.json());
+// app.use(express.urlencoded({extended: false}));
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
+
 
 app.get('/', (req, res) => {
   res.send('It is working!');
-})
-
-app.get('/favicon.ico', (req, res) => {
-  res.send('testing icon');
 })
 
 //REGISTER -> for REG, SIGN-IN, PROFILE_ID, IMAGE, we are doing dependency injection
