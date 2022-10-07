32e52c11b4ca3345466ebbb7f410a9e2721bce44
Daevah Taylaran <daevtayl@gmail.com>
2022-10-06 20:37:49 -0700

    clarifai

--- controllers/image.js
+++ controllers/image.js
@@ -1,17 +1,13 @@
 const Clarifai = require('clarifai');
 
-// const app = new Clarifai.App({
-//   apiKey: '0afee42ef93a497180797ad4650d128b'
-// });
-
-const app = new Clarifai.App({
+const clarifaiApp = new Clarifai.App({
   apiKey: '0afee42ef93a497180797ad4650d128b'
  });
 
  const handleApiCall = (req, res) => { 
-  app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+  clarifaiApp.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
   .then(data =>{res.json(data)})
-  .catch(err => res.status(400))
+  .catch(err => res.status(400).json('error'))
 }
 
 
