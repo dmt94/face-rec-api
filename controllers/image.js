682471997bb47a14ced99eed380a2b187902d251
Daevah Taylaran <daevtayl@gmail.com>
2022-10-06 18:42:34 -0700

    changed data

--- controllers/image.js
+++ controllers/image.js
@@ -50,8 +50,7 @@ const app = new Clarifai.App({
 const handleApiCall = (req, res) => {
     app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
     .then(data => {
-      // res.json(data)
-      console.log(data);
+      res.json(data)
     })
     .catch(err => res.status(400)
     .json('error'))
