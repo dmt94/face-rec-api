5d0dcedaadaef6430351cc362500bb3978ff22bd
Daevah Taylaran <daevtayl@gmail.com>
2022-10-06 18:54:13 -0700

    updated APIkey

--- controllers/image.js
+++ controllers/image.js
@@ -50,7 +50,8 @@ const app = new Clarifai.App({
 const handleApiCall = (req, res) => {
     app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
     .then(data => {
-      res.json(data)
+      console.log(data);
+      res.json(data);
     })
     .catch(err => res.status(400).json('error grabbing data'))
 }
