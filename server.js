acebe6f7ae7a3807e9697a2780c5d9f8ba784a8c
Daevah Taylaran <daevtayl@gmail.com>
2022-10-07 10:29:16 -0700

    check error

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.post('/imageurl', (req, res) => {
   })
   .catch(err => {
     // res.status(400).json(err)
-    res.json(CLARIFAI.FACE_DETECT_MODEL)
+    res.json(Clarifai.FACE_DETECT_MODEL)
   })
 })
 
