faab3a7cf3d992e509384b721e5ff2ca6049cb0c
Daevah Taylaran <daevtayl@gmail.com>
2022-10-06 20:30:43 -0700

    clarifai

--- controllers/image.js
+++ controllers/image.js
@@ -11,7 +11,7 @@ const app = new Clarifai.App({
  const handleApiCall = (req, res) => { 
   app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
   .then(data =>{res.json(data)})
-  .catch(err => res.status(400).json('error'))
+  .catch(err => res.status(400))
 }
 
 
