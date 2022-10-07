1133ccb32eb558522c75f2df1f187c2e19ff1e23
Daevah Taylaran <daevtayl@gmail.com>
2022-10-06 19:21:04 -0700

    idk

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
