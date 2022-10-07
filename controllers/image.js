6f4daaaec93ad3ae5ad3c858c8d0516ed787baf7
Daevah Taylaran <daevtayl@gmail.com>
2022-10-07 12:01:50 -0700

    image updates

--- controllers/image.js
+++ controllers/image.js
@@ -6,7 +6,9 @@ const app = new Clarifai.App({
 
 const handleApiCall = (req, res) => {
   app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
-    .then(data =>{res.json(data)})
+    .then(data =>{
+      res.json(data)
+    })
     .catch(err => res.status(400).json('unable to retrieve clarifai api data'))
 }
 
@@ -22,6 +24,6 @@ const handleImage = (req, res, db) => {
 }
 
 module.exports = {
-    handleImage:handleImage,
-    handleApiCall: handleApiCall
+    handleApiCall: handleApiCall,
+    handleImage:handleImage
 }
