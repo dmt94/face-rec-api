471c24541fb029e4cd6034f7bde232fa2396aa52
Daevah Taylaran <daevtayl@gmail.com>
2022-10-06 21:09:13 -0700

    clarifai

--- controllers/image.js
+++ controllers/image.js
@@ -5,9 +5,10 @@ const clarifaiApp = new Clarifai.App({
  });
 
  const handleApiCall = (req, res) => { 
-  clarifaiApp.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
-  .then(data => res.json(data))
-  .catch(err => res.status(400).json('unable to grab prediction'))
+  res.send.json(req.body.input);
+  // clarifaiApp.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+  // .then(data => res.json(data))
+  // .catch(err => res.status(400).json('unable to grab prediction'))
   // res.send(clarifaiApp.models.predict(Clarifai.FACE_DETECT_MODEL));
 }
 
